a0d4284141aae49a4a465e9b106d8f2992db4a1a
dggfdc <2042016556@qq.com>
2024-11-21 12:18:31 +0800

    Update planet_manager.h

--- include/planet_manager.h
+++ include/planet_manager.h
@@ -1,4 +1,42 @@
 #ifndef PLANET_MANAGER_H
 #define PLANET_MANAGER_H
 void hello();
+
+#define MAX_PLANETS 100
+#define MAX_NAME 50
+
+typedef struct {
+    char name[MAX_NAME];
+    char type[50];
+    double size;
+    double distance;
+    char details[200];
+} Planet;
+
+extern Planet catalog[MAX_PLANETS];
+extern int planetCount;
+
+void addPlanet();
+void editPlanet();
+void deletePlanet();
+void viewPlanet();
+#define MAX_PLANETS 100
+#define MAX_NAME 50
+
+typedef struct {
+    char name[MAX_NAME];
+    char type[50];
+    double size;
+    double distance;
+    char details[200];
+} Planet;
+
+extern Planet catalog[MAX_PLANETS];
+extern int planetCount;
+
+void addPlanet();
+void editPlanet();
+void deletePlanet();
+void viewPlanet();
+
 #endif //PLANET_MANAGER_H
