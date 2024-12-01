d3e44360bf4d5bc671c5e7c71c8dece192c925b6
Marcelino2005 <marcelino2005@163.com>
2024-12-01 19:01:55 +0800

    Update statics_manager.h

--- include/statics_manager.h
+++ include/statics_manager.h
@@ -3,15 +3,14 @@
 
 #include"searchplanet_manager.h" //包含searchplanet_manager.h
 
-void calculateStatistics(Planet planets[], int count, Statistics* stats);
-void displayStatistics(const Statistics* stats);
-
-
 //定义一个结构体用于储存
 typedef struct {
-	int catalogedPlanets;
-	int discoveredPlanets;
-	int observedPlanets;
+    int catalogedPlanets;
+    int discoveredPlanets;
+    int observedPlanets;
 }Statistics;
+
+void calculateStatistics(Planet planets[], int count, Statistics* stats);
+void displayStatistics(const Statistics* stats);
 
 #endif //SEARCH_MANAGER_H
